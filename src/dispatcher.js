31bc1350da75aeb167db29f9f03724895a352a16
Juan Carlos Lamas Alfaro <juank.yea@hotmail.com>
2021-05-23 00:50:31 -0400

    feat: add burden command

--- src/dispatcher.js
+++ src/dispatcher.js
@@ -1,5 +1,10 @@
+import { gitlabBurden } from './commads/gitlab_burden.js';
+
 export const dispatcher = ({ bot, userId, command, params = [] }) => {
   switch (command) {
+    case 'carga':
+      gitlabBurden({ bot });
+      break;
     default:
       bot.postMessageToChannel('general', `No te entiendo :sad-parrot:`);
   }
